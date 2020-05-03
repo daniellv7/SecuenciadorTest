610bcbe7e7834250af3aad3c9527acc7d1ff33ec
Daniel Larios <daniellv1992@gmail.com>
2020-05-02 21:06:07 -0500

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SecuenciadorTest
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal int CountNest { get; set; }
+        internal List<NestForm> NestList { get; set; }
+        internal List<Thread> ThreadList { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            NestList = new List<NestForm>();
+        }
+
+        private void btnMenuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        internal void FillNests()
+        {
+            for (int i = 0; i < CountNest; i++)
+            {
+                NestForm nestForm = new NestForm();
+                //Set the grid dimention
+                ColumnDefinition gridCol = new ColumnDefinition();
+                dynamicGrid.ColumnDefinitions.Add(gridCol);
+                //Add new form to list
+                NestList.Add(nestForm);
+                Frame frmMain = new Frame();
+                //Set the frame
+                frmMain.Content = NestList[i];
+                //Assign the form to the column
+                Grid.SetColumn(frmMain, i);
+                dynamicGrid.Children.Add(frmMain);
+            }
+        }
+
+        internal void Test()
+        {
+            for (int i = 0; i < CountNest; i++)
+            {
+                CreateThread();
+            }
+        }
+
+        internal void CreateThread()
+        {
+            Thread t1 = new Thread(new ThreadStart( CreateForm));
+            t1.SetApartmentState(ApartmentState.STA);
+            t1.Name = "Custom Thread";
+            t1.Start();
+        }
+
+        internal void CreateForm()
+        {
+            NestForm nestForm = new NestForm();
+            NestList.Add(nestForm);
+        }
+
+
+        private void menuCmbNest_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (menuCmbNest.SelectedItem != null || menuCmbNest.SelectedIndex != -1)
+            {
+                if (menuCmbNest.SelectedIndex == 0)
+                    CountNest = 1;
+                if (menuCmbNest.SelectedIndex == 1)
+                    CountNest = 2;
+                if (menuCmbNest.SelectedIndex == 2)
+                    CountNest = 3;
+                menuCmbVersion.IsEnabled = true;
+            }
+        }
+
+        private void menuCmbVersion_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (menuCmbVersion != null || menuCmbVersion.SelectedIndex != -1)
+            {
+                menuBtnStart.IsEnabled = true;
+                FillNests();
+            }
+        }
+
+        private void menuBtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            menuBtnStart.IsEnabled = true;
+            menuBtnStart.IsEnabled = false;
+            menuCmbNest.IsEnabled = false;
+            menuCmbVersion.IsEnabled = false;
+        }
+
+        
+    }
+}
---
+++ NestForm.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace SecuenciadorTest
+{
+    /// <summary>
+    /// Lógica de interacción para NestForm.xaml
+    /// </summary>
+    public partial class NestForm : Page
+    {
+        public int State { get; set; }
+
+        bool stateMachineStatus;
+
+        public NestForm()
+        {
+            InitializeComponent();
+            //
+            
+        }
+
+        internal void InitStateMachine()
+        {
+            while (stateMachineStatus)
+            {
+                switch (State)
+                {
+                    case 1: //Start
+                        break;
+                    case 2: //Stop
+                        break;
+                }
+            }
+        }
+
+        internal void StartSequence()
+        {
+
+        }
+
+        internal void FillListView()
+        {
+            List<Steps> items = new List<Steps>();
+            items.Add(new Steps() { Name = "Prueba numero 1", Number = 1, Status = "Passed" });
+            items.Add(new Steps() { Name = "Prueba numero 2", Number = 2, Status = "Passed" });
+            items.Add(new Steps() { Name = "Prueba numero 3", Number = 3, Status = "Failed" });
+            lvwSteps.ItemsSource = items;
+        }
+    }
+}
